c83a23f1283be68e6bd03df31d157b1112ff55bb
Piumal Dhanawardhana <24piumal@gmail.com>
2023-04-09 22:11:41 +0530

    initial

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <ul>
+            <li>Buy</li>
+            <li>Sell</li>
+            <li>Rent</li>
+            <li>Mortgage</li>
+            <li>Find Realtors</li>
+            <li>My Home</li>
+            <li>My Home</li>
+            <li>News & Insights</li>
+            <li>Advertise</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
